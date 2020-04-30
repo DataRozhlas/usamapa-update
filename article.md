c8fc8af56dca8a7ab24de531527a54ebbe5e5e6b
Nela Krawiecova <nela.krawiecova@gmail.com>
2020-04-30 16:56:43 +0200

    upravy-text

--- article.md
+++ article.md
@@ -1,7 +1,7 @@
 title: "Americký venkov krmí celou zemi, potýká se s koronavirem.<br>Trump masokombinátům přikázal nezavírat"
-perex: "Jedna třetina světové bilance nákažených Covid-19 připadá na Spojené státy. Epicentrem zůstává i nadále New York, nejvyšší počet nových případů ale hlásí zěmědělské státy Středozápadu. Nákaza se rychle šíří v masokombinátech, jejich vlastníci se obávají, že dojde k omezení některých produktů v supermarketech. Donald Trump proto podnikl zákonné kroky k udržení tohoto odvětví."
-coverimg: https://www.irozhlas.cz/sites/default/files/uploader/2020-04-28t223428z_1_200430-113423_nkr.JPG
-coverimg_note: "Kvůli zavřeným kavárnám či školám klesla cena i spotřeba mléka.<a href='https://reuters.com'>Reuters</a>"
+perex: "Milion nakažených, tedy jedna třetina ze všech, připadá na Spojené státy. Epicentrem zůstává i nadále New York, nejvyšší počty nových případů ale hlásí zěmědělské státy Středozápadu. Nákaza se rychle šíří v tamních masokombinátech, které omezují svou produkci. Může tedy dojít k omezení některých potravin v supermarketech. Donald Trump proto podnikl zákonné kroky k udržení tohoto odvětví."
+coverimg: https://www.irozhlas.cz/sites/default/files/styles/zpravy_snowfall/public/uploader/2020-04-28t223428z_1_200430-113423_nkr.JPG?itok=52Qh5zUs
+coverimg_note: "Kvůli zavřeným kavárnám či školám klesla v USA například cena i spotřeba mléka.<a href='https://reuters.com'>Reuters</a>"
 styles: []
 libraries: ['https://cdnjs.cloudflare.com/ajax/libs/d3/5.15.0/d3.min.js', "https://code.highcharts.com/maps/highmaps.js", "https://code.highcharts.com/mapdata/countries/us/custom/us-all-mainland.js", 'https://data.irozhlas.cz/corona-map/pocitadlo-usa.js'] #jquery, d3, highcharts, datatables
 options: [] #wide, noheader (, nopic)
@@ -12,30 +12,31 @@ options: [] #wide, noheader (, nopic)
 </wide>
 <br>
 
-Více než 500 lidí v masokombinátu Smithfield v Jižní Dakotě nemohlo přijít do práce, nakazili se koronavirem. Jedna z největších továren na zpracování vepřového se proto musela uzavřít. V sousední Iowě se počet pozitivních případů behěm 14 dní zvýšil desetkrát, většina z nich napojena na balírnu masa. 
+Více než 500 lidí v masokombinátu Smithfield v Jižní Dakotě nemohlo přijít do práce, nakazili se koronavirem. Jedna z největších továren na zpracování vepřového se proto musela uzavřít. V sousední Iowě se počet pozitivních případů behěm 14 dní zvýšil desetkrát, většina z nich napojena na balírnu masa. Počty obyvatel těchto států nejsou vysoké, v Iowě žije jen něco málo přes tři miliony Američanů, v potravinářském průmyslu ale pracuje podstatná část z nich. 
 
-Podle deníku [The New York Times](https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html) přerušilo nebo zpomalilo svou produkci nejméně 40 zařízení v potravinářském průmyslu. Kvůli nedostatku personálu se farmáři navíc obávají, že budou muset svá zvířata utratit.
+Podle deníku [The New York Times](https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html) přerušilo nebo zpomalilo svou produkci nejméně čtyřicet továren na potraviny po celé zemi. Kvůli nedostatku personálu se farmáři navíc obávají, že budou muset svá zvířata utratit. 
 
-Americký prezident Donald Trump proto kvůli obavám o udžení národní bezpečnosti uplatnil [zákon](https://www.ft.com/content/2c7e1a34-2cd7-4b80-ae2d-a8549f565423), který vládě dává pravomoc nařídit soukromým společnostem výrobu nepřerušovat. „Tento krok zajistí, aby firmy zapojené v potravinovém řetězci byly schopné bezpečně pokračovat ve výrobě a nedošlo k ohrožení potřeb Američanů,” znělo oficiální vyjádření Bílého domu.
+Americký prezident Donald Trump proto uplatnil [zákon](https://www.ft.com/content/2c7e1a34-2cd7-4b80-ae2d-a8549f565423), který vládě dává pravomoc nařídit soukromým společnostem výrobu nepřerušovat, a to kvůli obavám o udržení národní bezpečnosti. „Tento krok zajistí, aby nedošlo k ohrožení potřeb Američanů,” znělo oficiální vyjádření Bílého domu.
 
-Rozhodnutí přišlo poté, co největší americký masokombinát Tyson Foods varoval před kolapsem potravinového řetězce. Šíření viru mezi zaměstnanci donutilo společnost v posledním týdnu uzavřít tři svá zařízení.
+Rozhodnutí přišlo poté, co největší americký zpracovatel masa Tyson Foods varoval před kolapsem potravinového řetězce. Šíření viru mezi zaměstnanci donutilo společnost v posledním týdnu uzavřít tři svá zařízení. 
 
 <div id="corona_usa"></div>
 
-Podle Trumpa ale společnostem nyní nehrozí právní odpovědnost, pokud se během pandemie rozhodnout svá zařízení znovu otevřít. Odbory zastupující čtvrt milionů zaměstnanců toho odvětví se ale obávají, že není zajistěna jejich dostatečná bezpečnost. Odhadují, že se jich doposud koronavirem nakazilo šest a půl tisíce, dvě desítky zemřely.
+Podle Trumpa ale těmto závodům nehrozí právní odpovědnost, pokud se během pandemie rozhodnout znovu otevřít. Odbory zastupující čtvrt milionů zaměstnanců se ale obávají, že není zajistěna jejich dostatečná bezpečnost. Odhadují, že se jich doposud koronavirem nakazilo šest a půl tisíce, dvě desítky zemřely.
 
 ## Americký venkov krmí města
 
-Zatímco i nadále je nejvíce nakažených ve větších městech na pobřeží, v méně osídlených částech země může koronavirus paralyzovat i celé komunity. 
+Nejvíce nakažených je ve větších městech na pobřeží, v méně osídlených částech země ale koronavirus může paralyzovat celé komunity. 
 
 „Středozápad je sice méně obydlený, ale v továrnách jako jsou právě masokombináty jsou v úzkém kontaktu velké skupiny lidí z různých okolních vesnic. Proto se i tam začaly objevovat větší epicentra nákazy,“ uvedla pro server iROZHLAS.cz Lauryn Higgins, novinářka spolupracující s deníkem The New York Times z Nebrasky. Tamní zpracovatel hovězího JBS zaměstnává 3500 lidí, osm jeho zaměstanců nákaze koronavirem podlehlo.
 
-Pro americký venkov závislý na zemědělství navíc může být podle Higgins koronavirus velmi nebezpečný. „Zaměstnanci se obávají, že nakazí sebe i své blízké. Mnoho z nich si ale nemůže dovolit zůstat doma, navíc zavírání fabrik ohrožuje celé Spojené státy. Mají pocit, že musí lidem zajistit jídlo na stůl.“ 
+Pro americký venkov závislý na příjmech ze zemědělství navíc může být podle Higgins koronavirus velmi nebezpečný. „Lidé se obávají, že nakazí sebe i své blízké. Mnoho z nich si ale nemůže dovolit zůstat doma, navíc zavírání fabrik ohrožuje celé Spojené státy. Mají pocit, že musí lidem zajistit jídlo na stůl.“ 
 
-<img class="embed" src="https://www.irozhlas.cz/sites/default/files/uploader/2020-04-28t222601z_1_200430-114113_nkr.JPG" width="100%">
-<i> Koronavirus trápí také malé zemědělce, rodinná mlékárná ve státě Missouri musela kvůli nízké ceně i poptávce omezit svou výrobu. (Reuters)</i>
-<br>
-
+<figure>
+    <img src="https://www.irozhlas.cz/sites/default/files/styles/zpravy_clanek_cely/public/uploader/2020-04-28t222601z_1_200430-114113_nkr.JPG?itok=aYkEvT6_"
+         alt="">
+    <figcaption>Koronavirus trápí také malé zemědělce, rodinná mlékárná ve státě Missouri musela kvůli nízké ceně i poptávce omezit svou výrobu. (Reuters)</figcaption>
+</figure> <br>
 
 Není výjimkou, že na místech, kde potravinářské koncerny sídlí, bydlí jen několik stovek obyvatel. Pokud by se v takových komunitách nákaza masově rozšířila, může dojít k výpadku základních služeb. Obchody s potravinami, pošty, nebo pečovatelské domy mohou být vystaveny riziku, že nebudou mít dostatek zdravých zaměstnanců. 
 
@@ -43,6 +44,6 @@ Není výjimkou, že na místech, kde potravinářské koncerny sídlí, bydlí 
 
 ## Druhá zkouška
 
-Nakažených je ve Spojených státech přes milion, nejméně 60 tisíc lidí zemřelo. V dubnu se každý den ve Spojených státech v průměru nakazily dva tisíce lidí. Skutečný počet nakažených ale může být i několika násobně vyšší. Kolem 30 procent nemocných je ve státě New York, který je od počátku hlavním epicentrem nákazy. Nejpostiženějšími jsou New Jersey, Massachusetts, Kalifornie a Pensylvánie. Narůstající počet nových případů ale hlásí mnoho méně obydlených států - Iowa, Wisconsin, Nebraska nebo Kansas.
+Nakažených je ve Spojených státech přes milion, nejméně 60 tisíc lidí zemřelo. V dubnu se každý den ve Spojených státech v průměru nakazily dva tisíce lidí. Skutečné počty mohou být i několika násobně vyšší. Kolem 30 procent nemocných je ve státě New York, který je od počátku hlavním epicentrem nákazy. Nejpostiženějšími jsou New Jersey, Massachusetts, Kalifornie a Pensylvánie. Narůstající počet nových případů ale hlásí mnoho méně obydlených států - Iowa, Wisconsin, Nebraska nebo Kansas.
 
-Podle některých [odborníků](https://www.economist.com/graphic-detail/2020/04/25/the-south-is-likely-to-have-americas-highest-death-rate-from-covid-19) by koronavirus mohl výrazně postihnout také jih země. Hustě osídlená města na pobřežích mají širokou skladbu obyvatel, právě na venkově a americkém jihu je ale vyšší míra srdečních chorob nebo cukrovky, které mohou průběh nemoci komplikovat. 
+Podle některých [odborníků](https://www.economist.com/graphic-detail/2020/04/25/the-south-is-likely-to-have-americas-highest-death-rate-from-covid-19) by koronavirus mohl výrazně postihnout také jih země. Hustě osídlená města na pobřežích mají různorodější skladbu obyvatel, naopak na venkově a jihu země je ale průměrný věk obyvatel vyšší. Častější jsou také srdeční choroby nebo cukrovka, které mohou průběh nemoci komplikovat. 
--- output.js
+++ output.js
@@ -1,1 +1,112 @@
-!function(t){var e={};function n(r){if(e[r])return e[r].exports;var o=e[r]={i:r,l:!1,exports:{}};return t[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=t,n.c=e,n.d=function(t,e,r){n.o(t,e)||Object.defineProperty(t,e,{enumerable:!0,get:r})},n.r=function(t){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(t,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(t,"__esModule",{value:!0})},n.t=function(t,e){if(1&e&&(t=n(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)n.d(r,o,function(e){return t[e]}.bind(null,o));return r},n.n=function(t){var e=t&&t.__esModule?function(){return t.default}:function(){return t};return n.d(e,"a",e),e},n.o=function(t,e){return Object.prototype.hasOwnProperty.call(t,e)},n.p="",n(n.s=0)}([function(t,e,n){"use strict";n.r(e);n(1);fetch("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv").then((function(t){return t.text()})).then((function(t){var e=d3.csvParse(t,(function(t){return{datum:new Date(t.date),stat:t.state,fips:t.fips,z:Number(t.cases),umrti:Number(t.deaths)}})),n=[];e.forEach((function(t){return n.push(t.datum)}));var r=Math.max.apply(null,n),o=e.filter((function(t){return t.datum.valueOf()===r}));Highcharts.mapChart("containerusa",{chart:{borderWidth:0,map:"countries/us/custom/us-all-mainland"},title:{text:"Počet nakažených COVID-19 ve státech USA k "+new Date(r).getDate()+"."+(new Date(r).getMonth()+1)+"."+new Date(r).getFullYear()},subtitle:{text:"Pro přesný údaj o počtu nakažených a mrtvých vyberte stát"},credits:{text:"Zdroj dat: The New York Times",href:"https://github.com/nytimes/covid-19-data/",mapText:""},legend:{enabled:!1},mapNavigation:{enabled:!1},tooltip:{formatter:function(){return"<strong>".concat(this.point.properties.name,"</strong>: ").concat(this.point.z," potvrzených případů, ").concat(this.point.umrti," mrtvých")}},series:[{name:"States",color:"#d52834",enableMouseTracking:!1},{type:"mapbubble",name:"Potvrzené případy",color:"#d52834",data:o,joinBy:["woe-name","stat"],minSize:2,maxSize:"20%"}]})}))},function(t,e){if("Microsoft Internet Explorer"===navigator.appName||navigator.userAgent.match(/Trident/)||navigator.userAgent.match(/rv:11/)){var n=document.createElement("div");n.innerHTML='Používáte zastaralý Internet Explorer, takže vám části tohoto webu nemusí fungovat. Navíc to <a target="_blank" style="color:white;" rel="noopener noreferrer" href="https://www.zive.cz/clanky/microsoft-internet-explorer-neni-prohlizec-prestante-ho-tak-pouzivat/sc-3-a-197149/default.aspx">není bezpečné</a>, zvažte přechod na <a target="_blank" style="color:white;" rel="noopener noreferrer" href="https://www.mozilla.org/cs/firefox/new/">jiný prohlížeč</a>.',n.style.cssText="text-align:center;position:absolute;width:100%;height:auto;opacity:1;z-index:100;background-color:#d52834;top:37px;padding-top:4px;padding-bottom:3px;color:white;",document.body.appendChild(n)}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/script.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/byeie.js":
+/*!*********************!*\
+  !*** ./js/byeie.js ***!
+  \*********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("if (navigator.appName === \"Microsoft Internet Explorer\" || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/))) {\n  var warn = document.createElement(\"div\");\n  warn.innerHTML = \"Používáte zastaralý Internet Explorer, takže vám části tohoto webu nemusí fungovat. Navíc to <a target=\\\"_blank\\\" style=\\\"color:white;\\\" rel=\\\"noopener noreferrer\\\" href=\\\"https://www.zive.cz/clanky/microsoft-internet-explorer-neni-prohlizec-prestante-ho-tak-pouzivat/sc-3-a-197149/default.aspx\\\">není bezpečné</a>, zvažte přechod na <a target=\\\"_blank\\\" style=\\\"color:white;\\\" rel=\\\"noopener noreferrer\\\" href=\\\"https://www.mozilla.org/cs/firefox/new/\\\">jiný prohlížeč</a>.\";\n  warn.style.cssText = \"text-align:center;position:absolute;width:100%;height:auto;opacity:1;z-index:100;background-color:#d52834;top:37px;padding-top:4px;padding-bottom:3px;color:white;\";\n  document.body.appendChild(warn);\n}\n\n//# sourceURL=webpack:///./js/byeie.js?");
+
+/***/ }),
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _byeie__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./byeie */ \"./js/byeie.js\");\n/* harmony import */ var _byeie__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_byeie__WEBPACK_IMPORTED_MODULE_0__);\n // loučíme se s IE\n\nfetch(\"https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv\").then(function (response) {\n  return response.text();\n}).then(function (data) {\n  var result = d3.csvParse(data, function (d) {\n    return {\n      datum: new Date(d.date),\n      stat: d.state,\n      fips: d.fips,\n      z: Number(d.cases),\n      umrti: Number(d.deaths)\n    };\n  });\n  var datumy = [];\n  result.forEach(function (d) {\n    return datumy.push(d.datum);\n  });\n  var maxDatum = Math.max.apply(null, datumy);\n  var aktualniResult = result.filter(function (d) {\n    return d.datum.valueOf() === maxDatum;\n  });\n  Highcharts.mapChart('containerusa', {\n    chart: {\n      borderWidth: 0,\n      map: 'countries/us/custom/us-all-mainland'\n    },\n    title: {\n      text: 'Počet nakažených COVID-19 ve státech USA k ' + new Date(maxDatum).getDate() + '.' + (new Date(maxDatum).getMonth() + 1) + '.' + new Date(maxDatum).getFullYear()\n    },\n    subtitle: {\n      text: 'Pro přesný údaj o počtu nakažených a mrtvých vyberte stát'\n    },\n    credits: {\n      text: 'Zdroj dat: The New York Times',\n      href: 'https://github.com/nytimes/covid-19-data/',\n      mapText: ''\n    },\n    legend: {\n      enabled: false\n    },\n    mapNavigation: {\n      enabled: false\n    },\n    tooltip: {\n      formatter: function formatter() {\n        return \"<strong>\".concat(this.point.properties.name, \"</strong>: \").concat(this.point.z, \" potvrzen\\xFDch p\\u0159\\xEDpad\\u016F, \").concat(this.point.umrti, \" mrtv\\xFDch\");\n      }\n    },\n    series: [{\n      name: 'States',\n      color: '#d52834',\n      enableMouseTracking: false\n    }, {\n      type: 'mapbubble',\n      name: 'Potvrzené případy',\n      color: '#d52834',\n      data: aktualniResult,\n      joinBy: ['woe-name', 'stat'],\n      minSize: 2,\n      maxSize: '20%'\n    }]\n  });\n});\n\n//# sourceURL=webpack:///./js/script.js?");
+
+/***/ })
+
+/******/ });
